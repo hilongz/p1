088df15daca7e44677ed79b66cead63933e23013
hilongz <139072520+hilongz@users.noreply.github.com>
2023-07-09 14:43:57 -0400

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,14 @@
+# Corolla Price Analysis
+
+import pandas as pd
+
+# import data
+corolla = pd.DataFrame(pd.read_csv(r"C:\Users\Hailong\OneDrive\Desktop\DMV\Cars\corolla.csv"))
+# create age of car
+age = 2021 - corolla["Toyota Corolla"]
+# append age to table corolla
+corolla["Age"] = age
+
+corolla[["Age", "Mile", "Price"]].describe()
+
+
